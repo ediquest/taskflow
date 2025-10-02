2353b713693ba8716f1b042b0309ed8dfaff27fe
ediquest <a.sarczynski@gmail.com>
2025-10-02 13:29:37 +0200

    github_pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'taskflow'
+
 export default defineConfig({
   plugins: [react()],
+  // Dla "project pages":
+  base: `/${repoName}/`,
+
 })
